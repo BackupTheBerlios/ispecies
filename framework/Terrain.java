bb73b484fa85bb9e26a1e97d7a7f1b0696da3067
quintesse <quintesse>
2002-11-02 12:41:47 +0000

    Can now ask for Shapes and Tilesets.

--- framework/Terrain.java
+++ framework/Terrain.java
@@ -22,33 +22,74 @@ public class Terrain {
 	protected int m_nObstacles;
 	
 	protected Visual m_visual;
+	protected ResourceManager m_rm;
+	protected ImageStrip m_tiles;
+	protected int m_nShape;
+	protected int m_nTileSet;
 	
 	public Color color = Color.black;
 	
 	public Visual getVisual() { return m_visual; }
 	public void setVisual(Visual _visual) { m_visual = _visual; }
+
+	public int getShape() { return m_nShape; }
+	public void setShape(int _nShape) {
+		m_nShape = _nShape;
+		((IndexedVisual)m_visual).setIndex(_nShape);
+	}
+
+	public int getTileSet() { return m_nTileSet; }
+	public void setTileSet(int _nTileSet) {
+		m_nTileSet = _nTileSet;
+		m_tiles = m_rm.getTileSet(_nTileSet);
+		m_visual = new IndexedVisual(m_tiles, m_nShape);
+	}
+	
+	Terrain(ResourceManager _rm, int _nTileSet, int _nShape) {
+		m_rm = _rm;
+		m_nShape = _nShape;
+		setTileSet(_nTileSet);
+	}
+
+	static public Terrain getShapedTerrain(ResourceManager rm, int nTileSet, int nShape) {
+		Terrain t = null;
+		
+		switch (nTileSet) {
+			case 0:
+				t = new GrassTerrain(rm, 0, nShape);
+				break;
+			case 1:
+				t = new WaterTerrain(rm, 1, nShape);
+				break;
+			case 2:
+				t = new DesertTerrain(rm, 2, nShape);
+				break;
+		}
+		
+		return t;
+	}
 }
 
 class GrassTerrain extends Terrain {
-	GrassTerrain()
-	{
-		super();
+
+	GrassTerrain(ResourceManager _rm, int _nTileSet, int _nShape) {
+		super(_rm, _nTileSet, _nShape);
 		color = Color.green;
 	}
 }
 
 class WaterTerrain extends Terrain {
-	WaterTerrain()
-	{
-		super();
+
+	WaterTerrain(ResourceManager _rm, int _nTileSet, int _nShape) {
+		super(_rm, _nTileSet, _nShape);
 		color = Color.blue;
 	}
 }
 
 class DesertTerrain extends Terrain {
-	DesertTerrain()
-	{
-		super();
+
+	DesertTerrain(ResourceManager _rm, int _nTileSet, int _nShape) {
+		super(_rm, _nTileSet, _nShape);
 		color = Color.yellow;
 	}
 }
