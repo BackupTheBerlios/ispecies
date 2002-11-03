2f9316c4fc665e161ebcb8076931718893951dac
puf <puf>
2002-11-03 08:40:21 +0000

    Reformatted code

--- framework/MapBuilder.java
+++ framework/MapBuilder.java
@@ -3,28 +3,24 @@ import java.io.*;
 import java.awt.*;
 import java.util.*;
 
-public class MapBuilder
-{
+public class MapBuilder {
 	ResourceManager m_rm;
-
+	
 	MapBuilder(ResourceManager _rm) {
 		m_rm = _rm;
 	}
-
-	GameMap readGameMap(String _sFilename)
-		throws FileNotFoundException
-	{
+	
+	GameMap readGameMap(String _sFilename) throws FileNotFoundException {		
 		return readGameMap(new DataInputStream(new FileInputStream("res/maps/" + _sFilename)));
+		//return readGameMap(new DataInputStream(getClass().getResourceAsStream("res/maps/" + _sFilename)));
 	}
-
-	GameMap readGameMap(DataInputStream _in)
-	{
+	
+	GameMap readGameMap(DataInputStream _in) {
 		long width, height;
 		int  pwidth, pheight;
 		Map map = null;
-
-		try
-		{
+		
+		try {
 			StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(_in)));
 			st.eolIsSignificant(false);
 			st.commentChar('#');
@@ -37,51 +33,44 @@ public class MapBuilder
 			pwidth = (int)st.nval;
 			st.nextToken();
 			pheight = (int)st.nval;
-
+			
 			// create GameMap
 			map = new GameMap(width, height, pwidth, pheight);
-
+			
 			// read resource table
 			readResources(st, m_rm);
-
+			
 			// read parcel map
 			readParcelMap(st, map.getParcelMap());
-
+			
 			// read persisted game objects
 		}
-		catch(IOException e)
-		{
+		catch(IOException e) {
 			System.out.println("Invalid map file format");
 		}
 		return (GameMap)map;
 	}
-
-	void writeGameMap(String _sFilename, GameMap _game)
-		throws IOException
-	{
+	
+	void writeGameMap(String _sFilename, GameMap _game) throws IOException {
 		PrintWriter fl = new PrintWriter(new BufferedWriter(new FileWriter(_sFilename)));
 		writeGameMap(fl, _game);
 		fl.flush();
 		fl.close();
 	}
-
-	public void writeGameMap(PrintWriter _fl, GameMap _game)
-		throws IOException
-	{
-
+	
+	public void writeGameMap(PrintWriter _fl, GameMap _game) throws IOException {
+		
 		_fl.println("# width and height of the map in game units");
 		_fl.println(_game.getWidth() + " " + _game.getHeight());
 		_fl.println("# width and height of a parcel");
 		_fl.println(_game.mParcelWidth + " " + _game.mParcelHeight);
-
+		
 		writeResources(_fl, m_rm);
-
+		
 		writeParcelMap(_fl, _game.getParcelMap());
 	}
-
-	public void readResources(StreamTokenizer _st, ResourceManager _rm)
-		throws IOException
-	{
+	
+	public void readResources(StreamTokenizer _st, ResourceManager _rm) throws IOException {
 		_st.eolIsSignificant(false);
 		_st.commentChar('#');
 		while (_st.nextToken() != StreamTokenizer.TT_EOF) {
@@ -100,7 +89,7 @@ public class MapBuilder
 			}
 		}
 	}
-
+	
 	public void writeResources(PrintWriter _fl, ResourceManager _rm) {
 		_fl.println("#");
 		_fl.println("# Resources");
@@ -111,16 +100,13 @@ public class MapBuilder
 		}
 		_fl.println("end");
 	}
-
+	
 	public void readParcelMap(StreamTokenizer _st, ParcelMap _map)
-		throws IOException
-	{
+	throws IOException {
 		_st.eolIsSignificant(false);
 		_st.commentChar('#');
-		for (int y=0; y < _map.getHeight(); y++)
-		{
-			for (int x=0; x < _map.getWidth(); x++)
-			{
+		for (int y=0; y < _map.getHeight(); y++) {
+			for (int x=0; x < _map.getWidth(); x++) {
 				if (_st.nextToken() == StreamTokenizer.TT_NUMBER) {
 					int nType = (int)_st.nval;
 					if (_st.nextToken() == StreamTokenizer.TT_WORD) {
@@ -134,17 +120,13 @@ public class MapBuilder
 			} // for x
 		} // for y
 	}
-
-	public void writeParcelMap(PrintWriter _fl, ParcelMap _map)
-		throws IOException
-	{
+	
+	public void writeParcelMap(PrintWriter _fl, ParcelMap _map) throws IOException {
 		_fl.println("#");
 		_fl.println("# ParcelMap (Type/Shape/Height)");
 		_fl.println("#");
-		for (int y=0; y < _map.getHeight(); y++)
-		{
-			for (int x=0; x < _map.getWidth(); x++)
-			{
+		for (int y=0; y < _map.getHeight(); y++) {
+			for (int x=0; x < _map.getWidth(); x++) {
 				Parcel p = _map.getParcel(x,y);
 				Terrain t = p.getTerrain();
 				String s = String.valueOf(t.getTileSet()) + (char)(t.getShape() + (int)'a') + String.valueOf(p.getBaseHeight());
