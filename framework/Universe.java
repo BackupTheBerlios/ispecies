5b33bde5e70369d622eb70b3f97f51514ef68c33
quintesse <quintesse>
2002-11-05 15:31:50 +0000

    Using Logger.log() instead of System.out.writeln(); Added Javadoc comments. Added CVS history section.

--- framework/Universe.java
+++ framework/Universe.java
@@ -1,64 +1,112 @@
-//
-//
-// Universe
-//
-//
+/*
+ *  Universe.java
+*/
 
 import java.io.*;
 import java.util.*;
 import java.awt.*;
 
-class Universe extends Thread
-// Game state object
-{
-	protected GameMap			map;
-	protected ResourceManager	rm;
+/**
+ *  Umbrella object for all game-related objects and data
+ *
+ *@created    5 november 2002
+ */
+class Universe extends Thread {
 
-	public final int IMG_DOT =	1;
+	/**
+	 *  The "physical" part of the Universe
+	 */
+	protected GameMap map;
 
-	public TimerTriggerPool	heartBeat; // counts game ticks, not real time
+	/**
+	 *  The manager of the resources available in this Universe
+	 */
+	protected ResourceManager rm;
 
-	Universe()
-	{
+	/**
+	 *  Image resource Id for a red dot
+	 */
+	public final int IMG_DOT = 1;
+
+	/**
+	 *  The object that measures the passage of time
+	 */
+	public TimerTriggerPool heartBeat;
+	// counts game ticks, not real-time
+
+
+	/**
+	 *  Constructor for the Universe object
+	 */
+	Universe() {
 		heartBeat = new TimerTriggerPool();
 		rm = new ResourceManager();
 		rm.registerImage(IMG_DOT, "dot.gif");
 		map = readMap("Terrain.map");
 	}
 
+
+	/**
+	 *  Returns a reference to the map
+	 *
+	 *@return    The map
+	 */
 	GameMap getMap() {
 		return map;
 	}
 
+
+	/**
+	 *  Sets a new map for this Universe
+	 *
+	 *@param  _map  The new map
+	 */
 	void setMap(GameMap _map) {
 		map = _map;
 	}
 
+
+	/**
+	 *  Reads the map from a file
+	 *
+	 *@param  _mapFilename  The name of the Map to read (ignored for now)
+	 *@return               The Map that was read
+	 */
 	GameMap readMap(String _mapFilename) {
 		try {
 			return new MapBuilder(rm).readGameMap("Terrain.map");
-		}
-		catch (IOException e){
-			System.err.println("Could not read map: "+e.getMessage());
+		} catch (IOException e) {
+			Logger.log("Could not read map: " + e.getMessage());
 			e.printStackTrace(System.err);
 		}
 		return null;
 	}
 
-	public void run()
-	{
-		while (true)
-		{
-			try
-			{
-				sleep(50); // 50 msec per tick = 20 ticks per sec
+
+	/**
+	 *  Main processing method for the Universe object
+	 */
+	public void run() {
+		while (true) {
+			try {
+				sleep(50);
+				// 50 msec per tick = 20 ticks per sec
 				heartBeat.tick();
-			}
-			catch (InterruptedException e)
-			{
-				System.out.println("ServerUniverse: somebody woke me, "+e);
+			} catch (InterruptedException e) {
+				Logger.log("ServerUniverse: somebody woke me, " + e);
 			}
 		}
 	}
-} // Universe
+
+}
+
+/*
+ *  Revision history, maintained by CVS.
+ *  $Log: Universe.java,v $
+ *  Revision 1.4  2002/11/05 15:31:50  quintesse
+ *  Using Logger.log() instead of System.out.writeln();
+ *  Added Javadoc comments.
+ *  Added CVS history section.
+ *
+ */
 
