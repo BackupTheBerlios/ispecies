fb023a7acc4989f24b481e7bef7430eff3595555
quintesse <quintesse>
2002-11-05 15:33:37 +0000

    First check-in of a globally usable Logger class.

---
+++ framework/Logger.java
@@ -0,0 +1,33 @@
+/*
+ *  Logger.java
+ */
+ 
+import java.io.*;
+
+/**
+ *  Description of the Class
+ *
+ *@author     Tako
+ *@created    5 november 2002
+ */
+class Logger {
+
+	/**
+	 *  Writes a message to the log
+	 *
+	 *@param  _msg  The message to write to the log
+	 */
+	public static void log(String _msg) {
+		System.out.println(_msg);
+	}
+
+}
+
+/*
+ *  Revision history, maintained by CVS.
+ *  $Log: Logger.java,v $
+ *  Revision 1.1  2002/11/05 15:33:37  quintesse
+ *  First check-in of a globally usable Logger class.
+ *
+ */
+
