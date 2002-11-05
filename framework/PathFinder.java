a284963dce463391631e335af04583d9a70cc39a
puf <puf>
2002-11-05 20:33:35 +0000

    Now makes use of the functions in StringHelper and MathHelper. Started working on PathFinderDirector, which will try to find a path in a more intelligent way.

--- framework/PathFinder.java
+++ framework/PathFinder.java
@@ -67,28 +67,6 @@ public class PathFinder extends BaseGameObject implements Targettable, TimerRece
 		}
 	}
 	
-	// rotates the vector [_v] by [_a] degrees
-	protected Point rotate(Point _v, int _a) {
-		double a = Math.toRadians(_a);
-		Point result = new Point(
-			(int)Math.round(_v.x * Math.cos(a) + _v.y * Math.sin(a)),
-			(int)Math.round(_v.y * Math.cos(a) - _v.x * Math.sin(a))
-		);
-		//log("rotate("+_v+", "+_a+") = "+result);
-		return result;
-	}
-
-	// rotates the vector [_v] by [_a] degrees
-	protected FloatPoint rotate(FloatPoint _v, int _a) {
-		double a = Math.toRadians(_a);
-		FloatPoint result = new FloatPoint(
-			_v.x * Math.cos(a) + _v.y * Math.sin(a),
-			_v.y * Math.cos(a) - _v.x * Math.sin(a)
-		);
-		//log("rotate("+_v+", "+_a+") = "+result);
-		return result;
-	}
-
 	public void setPosition(FloatPoint _pos) {
 		super.setPosition(_pos);
 		if (mGame != null) {
@@ -111,25 +89,24 @@ public class PathFinder extends BaseGameObject implements Targettable, TimerRece
 		Logger.log(getName()+": "+_line);
 	}
 	
-	public String toString(Point _p) {
-		return "(" + _p.x + ", " + _p.y + ")";
-	}
-	
-	public String toString(FloatPoint _p) {
-		//return "(" + _p.x + ", " + _p.y + ")";
-		return "(" + toString(_p.x) + ", " + toString(_p.y) + ")";
-	}
-	
-	public String toString(double _f) {
-		return Integer.toString((int)_f)+"."+Integer.toString((int)(_f * 100 % 100));
-	}
-	
 }
 
+/** A <code>Director</code> is an object that determines where the <code>GameObject</code> moves.
+ */
 interface Director {
-	// Determines the direction to move in to get fastest from [_pos] to [_target] on [_map] at [_maxSpeed]
+	/** Determines the direction to move in to get fastest from [_pos] to [_target] on [_map] at [_maxSpeed].
+	 * @param _pos the position that the game object is currently at.
+	 * @param _target the position the game object is trying to reach.
+	 * @param _map the map over which the game object if moving.
+	 * @param _maxSpeed the maximum speed at which the object can move.
+	 * @return the direction in which the game object should move to reach it's destination according to this <code>Director</code>
+	 */
 	FloatPoint determineDirection(FloatPoint _pos, Point _target, GameMap _map, double _maxSpeed);
-	// Called to indicate that the game object has died at [_pos] on [_map]
+
+	/** Called to indicate that the game object has died at [_pos] on [_map]
+	 * @param _pos the position at which the game object has died.
+	 * @param the map on which the game object has died.
+	 */
 	public void onObjectDied(FloatPoint _pos, GameMap _map);
 }
 
@@ -152,40 +129,28 @@ class OneStepLookAheadDirector implements Director {
 		if (distance > _maxSpeed) {
 			direction.x = (_maxSpeed * direction.x / distance);
 			direction.y = (_maxSpeed * direction.y / distance);
-			//log("Direction=("+direction.x+","+direction.y+")");
 		}
 		// determine the cost to move ahead, left or right
 		double costAhead = getCostForMove(_pos, direction, _map);
-		double costLeft = getCostForMove(_pos, rotate(direction, -90), _map);
-		double costRight = getCostForMove(_pos, rotate(direction, 90), _map);
+		double costLeft = getCostForMove(_pos, MathHelper.rotate(direction, -90), _map);
+		double costRight = getCostForMove(_pos, MathHelper.rotate(direction, 90), _map);
 		if (costAhead > costLeft || costAhead > costRight) {
 			Point point = _map.gameXYToParcelXY(_pos.x, _pos.y);
-			log("Cost for moving from "+toString(_pos)+": ahead"+toString(direction)+"="+toString(costAhead)+" left="+toString(costLeft)+" right="+toString(costRight));
+			log("Cost for moving from "+StringHelper.toString(_pos)+": ahead"+StringHelper.toString(direction)+"="+StringHelper.toString(costAhead)+" left="+StringHelper.toString(costLeft)+" right="+StringHelper.toString(costRight));
 			if (costLeft < costRight) {
 				log("Stepping left");
-				direction = rotate(direction, -90);
+				direction = MathHelper.rotate(direction, -90);
 			}
 			else if (costRight < costLeft) {
 				log("Stepping right");
-				direction = rotate(direction, 90);
+				direction = MathHelper.rotate(direction, 90);
 			}
 			else {
 				log("Stepping left/right");
-				direction = ( (Math.random() < 0.5) ? rotate(direction, -90) : rotate(direction, 90) );
+				direction = ( (Math.random() < 0.5) ? MathHelper.rotate(direction, -90) : MathHelper.rotate(direction, 90) );
 			}
 		}
 		return direction;
-	}
-	
-	// rotates the vector [_v] by [_a] degrees
-	protected FloatPoint rotate(FloatPoint _v, int _a) {
-		double a = Math.toRadians(_a);
-		FloatPoint result = new FloatPoint(
-			_v.x * Math.cos(a) + _v.y * Math.sin(a),
-			_v.y * Math.cos(a) - _v.x * Math.sin(a)
-		);
-		//log("rotate("+_v+", "+_a+") = "+result);
-		return result;
 	}
 	
 	// Called to indicate that the game object has died at [_pos] on [_map]
@@ -204,24 +169,106 @@ class OneStepLookAheadDirector implements Director {
 	public void log(String _line) {
 		Logger.log("OneStepLookAheadDirector: "+_line);
 	}
-	
-	public String toString(Point _p) {
-		return "(" + _p.x + ", " + _p.y + ")";
+}
+
+class PathFinderDirector implements Director {
+	public static int[][] FLAGMAP = null;
+
+	public static synchronized int[][] getFLAGMAP(GameMap _map) {
+		// initialize flag map
+		if (FLAGMAP == null) {
+			FLAGMAP = new int[_map.mParcelMapWidth][_map.mParcelMapHeight];
+		}
+		return FLAGMAP;
 	}
 	
-	public String toString(FloatPoint _p) {
-		//return "(" + _p.x + ", " + _p.y + ")";
-		return "(" + toString(_p.x) + ", " + toString(_p.y) + ")";
+	class PathNode {
+		// The position this node represents.
+		FloatPoint mPosition;
+		// The node from which we can most cheaply reach this node
+		PathNode mCheapestPreviousNode;
+		// The lowest cost with which we can reach this node (from mCheapestPreviousNode)
+		double mLowestCost;
+		// The nodes we can reach from this node.
+		//    0
+		// 1  o  2
+		PathNode mNextNodes[] = new PathNode[3];
 	}
 	
-	public String toString(double _f) {
-		return Integer.toString((int)_f)+"."+Integer.toString((int)(_f * 100 % 100));
+	// Returns the lowest possible cost to get from [_p] to [_target]
+	double getLowestPotentialCost(FloatPoint _p, FloatPoint _target) {
+		return _p.distance(_target);
 	}
+	
+	PathNode findNodeWithLowestPotentialCost(PathNode _startNode) {
+		PathNode result = _startNode;
+		// TODO: recursively scan PathNodes
+		return result;
+	}
+
+	// Determines the direction to move in to get fastest from [_pos] to [_target] on [_map] at [_maxSpeed]
+	public FloatPoint determineDirection(FloatPoint _pos, Point _target, GameMap _map, double _maxSpeed) {
+		return null;
+	}
+	
+	public FloatPoint _determineDirection(FloatPoint _pos, Point _target, GameMap _map, double _maxSpeed) {
+		FloatPoint direction = new FloatPoint(_target.x - _pos.x, _target.x - _pos.y);
+		double distance = _pos.distance(_target.getX(), _target.getY());
+		// if distance > speed, scale vector down to speed
+		if (distance > _maxSpeed) {
+			direction.x = (_maxSpeed * direction.x / distance);
+			direction.y = (_maxSpeed * direction.y / distance);
+		}
+		// determine the cost to move ahead, left or right
+		double costAhead = getCostForMove(_pos, direction, _map);
+		double costLeft = getCostForMove(_pos, MathHelper.rotate(direction, -90), _map);
+		double costRight = getCostForMove(_pos, MathHelper.rotate(direction, 90), _map);
+		if (costAhead > costLeft || costAhead > costRight) {
+			Point point = _map.gameXYToParcelXY(_pos.x, _pos.y);
+			log("Cost for moving from "+StringHelper.toString(_pos)+": ahead"+StringHelper.toString(direction)+"="+StringHelper.toString(costAhead)+" left="+StringHelper.toString(costLeft)+" right="+StringHelper.toString(costRight));
+			if (costLeft < costRight) {
+				log("Stepping left");
+				direction = MathHelper.rotate(direction, -90);
+			}
+			else if (costRight < costLeft) {
+				log("Stepping right");
+				direction = MathHelper.rotate(direction, 90);
+			}
+			else {
+				log("Stepping left/right");
+				direction = ( (Math.random() < 0.5) ? MathHelper.rotate(direction, -90) : MathHelper.rotate(direction, 90) );
+			}
+		}
+		return direction;
+	}
+	
+	// Called to indicate that the game object has died at [_pos] on [_map]
+	public void onObjectDied(FloatPoint _pos, GameMap _map) {
+		Point point = _pos.toPoint();
+		Point parcelposition = _map.gameXYToParcelXY(point.x, point.y);
+		getFLAGMAP(_map)[parcelposition.x][parcelposition.y]++;
+	}
+
+	// determines the cost for an object at [_pos] to move in [_dir]
+	protected double getCostForMove(Point2D.Double _pos, Point2D.Double _dir, GameMap _map) {
+		Point2D.Double dst = new Point2D.Double(_pos.x + _dir.x, _pos.y + _dir.y);
+		Point parcelindex = _map.gameXYToParcelXY((int)dst.x, (int)dst.y);
+		return getFLAGMAP(_map)[parcelindex.x][parcelindex.y] * 1000 + _pos.distance(dst);
+	}
+
+	public void log(String _line) {
+		Logger.log("PathFinderDirector: "+_line);
+	}
+	
 }
 
 /*
  *  Revision history, maintained by CVS.
  *  $Log: PathFinder.java,v $
+ *  Revision 1.6  2002/11/05 20:33:35  puf
+ *  Now makes use of the functions in StringHelper and MathHelper.
+ *  Started working on PathFinderDirector, which will try to find a path in a more intelligent way.
+ *
  *  Revision 1.5  2002/11/05 15:27:48  quintesse
  *  Using Logger.log() instead of System.out.writeln();
  *  Added CVS history section.
