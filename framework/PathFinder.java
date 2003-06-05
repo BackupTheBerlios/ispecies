d40fbf9cb6db1330e32f614a725dfb4a08db179a
puf <puf>
2003-06-05 15:24:51 +0000

    Removed the OneStepLookAheadDirector class, which is now in a file of it's own. Removed dependency on JDK1.4 logging mechanism.

--- framework/PathFinder.java
+++ framework/PathFinder.java
@@ -3,8 +3,6 @@
  *
  */
 import java.awt.*;
-import java.awt.geom.*;
-import java.util.logging.*;
 
 import util.*;
 
@@ -14,8 +12,6 @@ import util.*;
  * @author  puf
  */
 public class PathFinder extends BaseGameObject implements Targettable, TimerReceiver {
-	static Logger mLogger = Logger.getLogger(PathFinder.class.getName());
-
 	private Point mTarget = null;
 	private Universe mGame = null;
 	private TimerTrigger mTrigger;
@@ -54,26 +50,33 @@ public class PathFinder extends BaseGameObject implements Targettable, TimerRece
 	}
 	
 	public void doTimer(TimerTrigger timerTrigger) {
-		//log("received timer trigger");
 		if (mPosition != null && mTarget != null) {
 			// determine direction and distance of movement
 			FloatPoint pos = new FloatPoint(getPosition());
 			FloatPoint direction = mDirector.determineDirection(pos, mTarget, mGame.getMap(), mSpeed);
-			// update position according to direction and clip to map size
-			pos.x += direction.x;
-			if (pos.x < 0) {
-				pos.x = 0;
-			} else if (pos.x >= mMap.mMapWidth) {
-				pos.x = (int)mMap.mMapWidth-1;
+			if (direction != null) {
+				// update position according to direction and clip to map size
+				pos.x += direction.x;
+				if (pos.x < 0) {
+					pos.x = 0;
+				} else if (pos.x >= mMap.mMapWidth) {
+					pos.x = (int)mMap.mMapWidth-1;
+				}
+				pos.y += direction.y;
+				if (pos.y < 0) {
+					pos.y = 0;
+				} else if (pos.y >= mMap.mMapHeight) {
+					pos.y = (int)mMap.mMapHeight-1;
+				}
+				// update the position field
+				setPosition(pos);
+				// determine if we've reached the target
+				if (mTarget.equals(pos.toPoint())) {
+					log("Reached target");
+					mTarget = null;
+					mDirector = null;
+				}
 			}
-			pos.y += direction.y;
-			if (pos.y < 0) {
-				pos.y = 0;
-			} else if (pos.y >= mMap.mMapHeight) {
-				pos.y = (int)mMap.mMapHeight-1;
-			}
-			// update the position field
-			setPosition(pos);
 		}
 	}
 	
@@ -95,80 +98,20 @@ public class PathFinder extends BaseGameObject implements Targettable, TimerRece
 	}
 	
 	public void log(String _line) {
-		mLogger.info(getName()+": "+_line);
+		Logger.log(getName()+": "+_line);
+
 	}
 	
-}
 
-class OneStepLookAheadDirector implements Director, ObjectListener {
-	static Logger mLogger = Logger.getLogger(OneStepLookAheadDirector.class.getName());
-
-	public static int[][] FLAGMAP = null;
-
-	public synchronized int[][] getFLAGMAP(GameMap _map) {
-		// initialize flag map
-		if (FLAGMAP == null) {
-			FLAGMAP = new int[_map.mParcelMapWidth][_map.mParcelMapHeight];
-		}
-		return FLAGMAP;
-	}
-
-	// Determines the direction to move in to get fastest from [_pos] to [_target] on [_map] at [_maxSpeed]
-	public FloatPoint determineDirection(FloatPoint _pos, Point _target, GameMap _map, double _maxSpeed) {
-		FloatPoint direction = new FloatPoint(_target.x - _pos.x, _target.x - _pos.y);
-		double distance = _pos.distance(_target.getX(), _target.getY());
-		// if distance > speed, scale vector down to speed
-		if (distance > _maxSpeed) {
-			direction.x = (_maxSpeed * direction.x / distance);
-			direction.y = (_maxSpeed * direction.y / distance);
-		}
-		// determine the cost to move ahead, left or right
-		double costAhead = getCostForMove(_pos, direction, _map);
-		double costLeft = getCostForMove(_pos, MathHelper.rotate(direction, -90), _map);
-		double costRight = getCostForMove(_pos, MathHelper.rotate(direction, 90), _map);
-		if (costAhead > costLeft || costAhead > costRight) {
-			Point point = _map.gameXYToParcelXY(_pos.x, _pos.y);
-			log("Cost for moving from "+StringHelper.toString(_pos)+": ahead"+StringHelper.toString(direction)+"="+StringHelper.toString(costAhead)+" left="+StringHelper.toString(costLeft)+" right="+StringHelper.toString(costRight));
-			if (costLeft < costRight) {
-				log("Stepping left");
-				direction = MathHelper.rotate(direction, -90);
-			}
-			else if (costRight < costLeft) {
-				log("Stepping right");
-				direction = MathHelper.rotate(direction, 90);
-			}
-			else {
-				log("Stepping left/right");
-				direction = ( (Math.random() < 0.5) ? MathHelper.rotate(direction, -90) : MathHelper.rotate(direction, 90) );
-			}
-		}
-		return direction;
-	}
-	
-	// Called to indicate that the game object has died
-	public void died(ObjectEvent _e) {
-		BaseGameObject src = (BaseGameObject)_e.getSource();
-		Point point = src.getPosition().toPoint();
-		GameMap map = src.getMap();
-		Point parcelposition = map.gameXYToParcelXY(point.x, point.y);
-		getFLAGMAP(map)[parcelposition.x][parcelposition.y]++;
-	}
-
-	// determines the cost for an object at [_pos] to move in [_dir]
-	protected double getCostForMove(Point2D.Double _pos, Point2D.Double _dir, GameMap _map) {
-		Point2D.Double dst = new Point2D.Double(_pos.x + _dir.x, _pos.y + _dir.y);
-		Point parcelindex = _map.gameXYToParcelXY((int)dst.x, (int)dst.y);
-		return getFLAGMAP(_map)[parcelindex.x][parcelindex.y] * 1000 + _pos.distance(dst);
-	}
-	
-	public void log(String _line) {
-		mLogger.info("OneStepLookAheadDirector: "+_line);
-	}
 }
 
 /*
  *  Revision history, maintained by CVS.
  *  $Log: PathFinder.java,v $
+ *  Revision 1.11  2003/06/05 15:24:51  puf
+ *  Removed the OneStepLookAheadDirector class, which is now in a file of it's own.
+ *  Removed dependency on JDK1.4 logging mechanism.
+ *
  *  Revision 1.10  2002/11/12 08:34:16  quintesse
  *  Now using official 1.4 JDK logging system.
  *
